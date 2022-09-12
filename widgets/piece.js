6069a093f1099c4a3313701c97f867b2399ca009
Chris Nicoll <clutterstack@users.noreply.github.com>
2022-09-12 10:00:42 -0400

    fix refresh while typing in pins tiddler

--- widgets/piece.js
+++ widgets/piece.js
@@ -57,7 +57,7 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
         for (const pieceNode of this.pieceNodes) {
             this.pieceTitles.push(pieceNode.getAttribute("data-omnitid"));
         }
-        console.log("pieceTitles: " + this.pieceTitles);
+        //console.log("pieceTitles: " + this.pieceTitles);
         // Ensure sidecar tiddler has coords for all subtids
         this.initCoords();
         // Put nodes in the positions specified in the sidecar
@@ -70,6 +70,7 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
     pieceWidget.prototype.execute = function() {
         this.streamClass = this.getAttribute("class");
         this.omniTitle = this.getAttribute("omnititle");
+        this.isEditing = false;
         this.sidecar = this.omniTitle+"-sidecar";
         this.freshPosn = [10,10];
         // Make child widgets
@@ -79,7 +80,7 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
     
     pieceWidget.prototype.initCoords = function() {
         // get all the divs with class omni-item-container
-        console.log("initCoords")
+        //console.log("initCoords")
         // check for sidecar
         let sidecarData = this.wiki.getTiddlerData(this.sidecar);
         //console.log(`typeof(sidecarData): ${typeof(sidecarData)}`)
@@ -88,10 +89,10 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
         if (sidecarData != undefined) {
         // for each container, get its position from that data.
             for (const pieceTitle of this.pieceTitles) {
-                console.log("pieceTitle: " + pieceTitle);
+                //console.log("pieceTitle: " + pieceTitle);
                 // see if we can pull a coords value for the current piece
                 const extracteditem = sidecarData[pieceTitle];
-                console.log(`extracteditem: ${JSON.stringify(extracteditem)}`)
+                // console.log(`extracteditem: ${JSON.stringify(extracteditem)}`)
                 if (extracteditem == undefined ) {
                     this.wiki.setText(this.sidecar, undefined, pieceTitle, this.freshPosn);
                     this.freshPosn = [this.freshPosn[0] + 20, this.freshPosn[1] + 20];
@@ -111,14 +112,14 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
     }
 
     pieceWidget.prototype.placePieces = function () {
-        console.log("placePieces invoked")
+       // console.log("placePieces invoked")
         this.positions = this.wiki.getTiddlerData(this.sidecar);
         for (const pieceNode of this.pieceNodes) {
             const pieceTitle = pieceNode.getAttribute("data-omnitid");
             pieceNode.classList.remove("omni-relative");
             pieceNode.classList.add("omni-absolute");
             console.log(pieceTitle)
-            console.log(this.positions[pieceTitle][0],this.positions[pieceTitle][1])
+            //console.log(this.positions[pieceTitle][0],this.positions[pieceTitle][1])
             pieceNode.style.left=this.positions[pieceTitle][0]+"px";
             pieceNode.style.top=this.positions[pieceTitle][1]+"px";
             pieceNode.style.width="400px";
@@ -129,21 +130,30 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
         self = this;
         self.pieceNode.style.top=self.coords[1]+"px"; 
         self.pieceNode.style.left=self.coords[0]+"px"; 
-        console.log(`css position styles: left ${self.pieceNode.style.left}, top ${self.pieceNode.style.top}`)
+        //console.log(`css position styles: left ${self.pieceNode.style.left}, top ${self.pieceNode.style.top}`)
     }
 
     /*
     Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
     */
     pieceWidget.prototype.refresh = function(changedTiddlers) {
-        var changedAttributes = this.computeAttributes();
-        //console.log("about to refreshChildren")
-        //
-         if(changedAttributes) {
+        // To log changes in attributes:
+        // self=this;
+        // $tw.utils.each(self.parseTreeNode.attributes,function(attribute,name) {
+        //     var value = self.computeAttribute(attribute);
+        //     console.log("attribute " + name + ": old" + " = " + value + "; new = " + self.attributes[name]);
+        // });
+         var changedAttributes = this.computeAttributes();
+         console.log("in prototype.refresh")
+        
+         if(changedAttributes==true) {
+            console.log("have changed attrs; going to refreshSelf");
+            console.log(this.parseTreeNode.attributes);
              this.refreshSelf();
             return true;
          } 
          if(changedTiddlers) { // not sure if need this condition; was troubleshooting a "changedtiddlers is undefined" message in annot testing
+            console.log("have changedTiddlers; going to refresh children");
             return this.refreshChildren(changedTiddlers);
         }
     };
@@ -156,14 +166,14 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
         if (event.target.classList.contains("omni-pinhandle")) {
             this.pieceNode = event.target.closest(".omni-item-container");
             this.pieceTitle = this.pieceNode.getAttribute("data-omnitid");
-            console.log("data-omnitid: " + this.pieceTitle);
+            //console.log("data-omnitid: " + this.pieceTitle);
             //now the initial position of the target
             this.coords = [this.pieceNode.offsetLeft, this.pieceNode.offsetTop];
-            console.log("mousedown: offsetLeft = " + this.coords[0] + "offsetTop = " + this.coords[1])
+            //console.log("mousedown: offsetLeft = " + this.coords[0] + "offsetTop = " + this.coords[1])
             this.startDrag = [event.clientX,event.clientY];
-            console.log("mousedown, startDrag: " + this.startDrag)
+            // console.log("mousedown, startDrag: " + this.startDrag)
             this.waitingforframe = null;
-            console.log("mousedown: this.coords = "+ this.coords)
+            // console.log("mousedown: this.coords = "+ this.coords)
             // console.log(`start mouse drag: this.coords is ${this.coords} and the startDrag position is ${this.startDrag}`);
             this.domNode.removeEventListener("mousedown", this.handleMouseDown);
             this.domNode.addEventListener("mousemove", this.handleMouseMove);
@@ -180,14 +190,14 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
             this.waitingforframe = true;
             this.delta = [event.clientX - this.startDrag[0], event.clientY - this.startDrag[1] ]
             this.startDrag = [event.clientX, event.clientY];
-            console.log("mousemove: delta = " + this.delta);
+            //console.log("mousemove: delta = " + this.delta);
             window.requestAnimationFrame(this.movePiece.bind(this));
         }
     };
     
     pieceWidget.prototype.movePiece = function() {
         this.coords = [this.coords[0] + this.delta[0],this.coords[1] + this.delta[1]];
-        console.log(`mouse move: coords = ${this.coords}`)
+        //console.log(`mouse move: coords = ${this.coords}`)
         this.positionStyles();
         this.waitingforframe = null;
     };
@@ -195,10 +205,10 @@ Widget that can be moved around. Needs a positioned parent container, and omnibu
     pieceWidget.prototype.handleMouseUpEvent = function(event) {
         //event.preventDefault();
         //event.stopPropagation();
-        console.log("mouseup triggered on node "+ event.target.className)
+        //console.log("mouseup triggered on node "+ event.target.className)
         this.domNode.removeEventListener("mousemove", this.handleMouseMove);
-        this.wiki.setText(this.sidecar, undefined, this.pieceTitle, this.coords);
-            // console.log(`mouse drop: ${this.coords}.`);
+        this.wiki.setText(this.sidecar, undefined, this.pieceTitle, JSON.stringify(this.coords));
+             console.log(`mouse drop: ${this.coords}.`);
         this.domNode.removeEventListener("mouseup", this.handleMouseUp);
         this.domNode.removeEventListener("mouseleave", this.handleMouseUp);
         this.domNode.addEventListener("mousedown", this.handleMouseDown);
